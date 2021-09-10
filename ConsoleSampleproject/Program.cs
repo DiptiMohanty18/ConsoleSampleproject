0a2fb009507fd5171fbe98216ad17e8ee3ad52fd
diptimohanty18 <dipti.mohanty@dell.com>
2021-09-10 20:53:22 +0530

    some changes happened

--- ConsoleSampleproject/Program.cs
+++ ConsoleSampleproject/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleSampleproject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("How are you doing?");
             Console.ReadKey();
         }
     }
---
+++ ConsoleSampleproject/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSampleproject
+{
+    class Student
+    {
+        int id;
+        String name;
+        int age;
+    }
+}
